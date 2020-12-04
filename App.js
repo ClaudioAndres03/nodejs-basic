519a0e25a1362f7e6a43e93ac6379af5db7c13c5
Claudio Andres <ClaudioAndres03@outlook.com>
2020-12-04 14:19:18 -0400

    Package.json y NPM

--- App.js
+++ App.js
@@ -1,10 +1,20 @@
 
 const { frutero, dinero } = require('./frutas');
+const cowsay = require("cowsay")
 
-//  Exports y Require
+//  Package.json y NPM
+console.log(
+    cowsay.say({
+        text: "Klok Sicario Muuuuuu",
+        e: "Oo",
+        T: "U",
+    })
+);
+
 
 frutero.forEach(fruta => {
     console.count(fruta);
 });
 
 console.log(dinero);
+
