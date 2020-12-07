0c28e0bdd9b23ef5c23b2eb7a610450ecfae2845
Claudio Andres <ClaudioAndres03@outlook.com>
2020-12-07 11:38:56 -0400

    Introducción a Express.js

--- App.js
+++ App.js
@@ -1,13 +1,28 @@
-// Fundamentos de servidor HTTP
-const http = require('http');
-const server = http.createServer((req, res) => {
-    res.end('Estoy respondiendo tu solicitud v.3');
+// Introducción a Express.js
+
+const { static } = require('express');
+const express = require('express');
+const app = express();
+
+const port = 3000;
+
+app.use(static(__dirname + '/public'))
+
+app.get('/', (req, res) => {
+    res.send('Mi respuestas en Express')
+})
+
+app.get('/servicios', (req, res) => {
+    res.send('Esta es la pagina de servicios')
 }
 )
 
-
-const puerto = 3000;
-server.listen(puerto, () => {
-    console.log('Escuchando solicitudes')
+app.use((req, res, next) => {
+    res.status(404).sendFile(__dirname + '/public/404.html')
 }
 )
+
+app.listen(port, () => {
+    console.log('Servidor a su servicio en el puero', port)
+})
+
