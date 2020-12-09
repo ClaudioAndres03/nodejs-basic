655f70567d553c12ec0e356b8ff7333345bd8f60
Claudio Andres <ClaudioAndres03@outlook.com>
2020-12-09 10:05:06 -0400

    Template Engines (EJS) con Express

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-// Introducción a Express.js
+// Template Engines (EJS) con Express
 
 const { static } = require('express');
 const express = require('express');
@@ -6,21 +6,27 @@ const app = express();
 
 const port = 3000;
 
+// motor de plantillas
+app.set('view engine', 'ejs')
+app.set('views', __dirname + '/views')
+
+
 app.use(static(__dirname + '/public'))
 
 app.get('/', (req, res) => {
-    res.send('Mi respuestas en Express')
+    res.render("index", { titulo: "Mi titulo dinamico" })
 })
 
-app.get('/servicios', (req, res) => {
-    res.send('Esta es la pagina de servicios')
-}
-)
+app.get("/servicios", (req, res) => {
+    res.render('servicios', { tituloServicios: "Mi titulo dinamico de servicios" })
+})
 
 app.use((req, res, next) => {
-    res.status(404).sendFile(__dirname + '/public/404.html')
-}
-)
+    res.status(404).render('404', {
+        titulo: "Error 404",
+        description: "esta es la pagina 404"
+    })
+})
 
 app.listen(port, () => {
     console.log('Servidor a su servicio en el puero', port)
